9eb4e9dd13aa8d5a9ad4df5ad7a83b42b7832d82
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-22 12:59:31 -0800

    fixed error with server failing to produce errors despite express-validator size violations which was due to checking  instead of  - note the parentheses

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const { body, validationResult } = require("express-validator");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post("/", [
+  body("firstname", "First name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .isLength({ max: 100 })
+    .withMessage("First name must not exceed 100 characters.")
+    .escape(),
+  body("lastname", "Last name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .isLength({ max: 100 })
+    .withMessage("First name must not exceed 100 characters.")
+    .escape(),
+  (req, res, next) => {
+    // finds validation errors and wraps
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      // 400 indicates server will not process the request due to a perceived client error
+      return res.status(400).json({ errors: errors.array() });
+
+    // no validation errors
+    console.log(req.body);
+    res.redirect("http://127.0.0.1:3000/#contact");
+  },
+]);
 
 module.exports = router;
