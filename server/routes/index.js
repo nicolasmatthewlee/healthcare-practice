52895540c257d1a73c8e00ddcf82130243b4ad85
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-23 06:34:09 -0800

    added validationand sanitization for all form data

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,9 +20,31 @@ router.post("/", [
     .isLength({ max: 100 })
     .withMessage("First name must not exceed 100 characters.")
     .escape(),
+  body("email", "Please enter a valid email.")
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .isLength({ min: 1 })
+    .withMessage("Email must be not be empty.")
+    .isLength({ max: 100 })
+    .withMessage("Email must not exceed 100 characters.")
+    .escape(),
+  body("phone", "Please provide a valid phone number.")
+    .trim()
+    .isMobilePhone()
+    .escape(),
+  body("availability", "Please select a contact preference.")
+    .trim()
+    .isLength({ max: 100 })
+    .escape(),
+  body("message", "Message must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
   (req, res, next) => {
     // finds validation errors and wraps
     const errors = validationResult(req);
+    console.log(errors);
     if (!errors.isEmpty())
       // 400 indicates server will not process the request due to a perceived client error
       return res.status(400).json({ errors: errors.array() });
@@ -30,19 +52,18 @@ router.post("/", [
     // no validation errors
     console.log(req.body);
 
-    console.log(process.env.EMAIL, process.env.PASSWORD);
-
     // create email transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: { user: process.env.EMAIL, pass: process.env.PASSWORD },
     });
+
     // set message options
     const mailOptions = {
       from: process.env.Email,
       to: "nicolaslee2017@gmail.com",
       subject: "Your Message to PsychCorp",
-      text: "nice :)",
+      text: `Name: ${req.body.lastname}, ${req.body.firstname}\nEmail: ${req.body.email}\nPhone: ${req.body.phone}\nAvailability: ${req.body.availability}\nMessage:\n${req.body.message}`,
     };
     // send mail
     transporter.sendMail(mailOptions, (error, info) => {
